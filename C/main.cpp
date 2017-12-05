9ed1ac56d9ac853716c0674337d42ef37f12d733
ddkV587 <ddkV587@gmail.com>
2017-12-05 17:02:03 +0800

    update

---
+++ C/main.cpp
@@ -0,0 +1,16 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+
+static int socket_fd;
+
+void initSocket()
+{
+	socket_fd = socket(AF_INET, SOCK_STREAM, );
+
+}
+
+int main(int argc, const char *argv[])
+{
+	
+	return 0;
+}
