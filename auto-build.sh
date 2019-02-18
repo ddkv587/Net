1e7560387751b87d7ddd736bece1f58cd1595111
ddkV587 <ddkV587@gmail.com>
2019-02-18 16:34:23 +0800

    update

--- auto-build.sh
+++ auto-build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "============= start build NET =============="
-
+cd build
+make release
 
 echo "============= build NET done =============="
--- build/linux/BUILD_LIBRARY.mk
+++ build/linux/BUILD_LIBRARY.mk
@@ -3,7 +3,11 @@ BUILD_MIDDLE_PATH 	:= $(IM_PATH)/$(TARGET_MODULE)
 
 CPP_APP_OBJECT		:= $(foreach filename, $(CPP_SRC:%.cpp=%.o), $(subst $(CPP_SRCDIR), $(BUILD_MIDDLE_PATH), $(filename)))
 
-all: $(TARGET_MODULE)
+all: release debug
+
+release: $(TARGET_MODULE)
+
+debug: $(TARGET_MODULE)_debug
 
 $(CPP_APP_OBJECT): $(BUILD_MIDDLE_PATH)%.o: $(CPP_SRCDIR)%.cpp
 	@echo $(notdir $<)
