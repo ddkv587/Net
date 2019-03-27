2577e062420e0b85a87b204b76a5c99d4170f987
邓道宽 <532911875@qq.com>
2019-03-27 18:54:24 +0800

    Add files via upload

---
+++ recy/CMsgScreenTouch.cpp
@@ -0,0 +1,45 @@
+#include "OpenHMI_WindowSystem.h"
+
+namespace OpenHMI
+{
+	CMsgScreenTouch::CMsgScreenTouch(
+					IScreenTouchListener	*pListener,
+					const CCursor			&cursor)
+		: m_pListener(pListener)
+		, m_cursor(cursor)
+	{
+		m_eType = CBaseMessage::MT_SCREEN_TOUCH;
+		m_uiID = m_eType;
+	}
+
+	CMsgScreenTouch::~CMsgScreenTouch()
+	{
+		;
+	}
+
+	void CMsgScreenTouch::handleMessage()
+	{
+		if (m_pListener == NULL)
+		{
+			return;
+		}
+
+		switch (m_cursor.getTouchStatus())
+		{
+		case CCursor::TS_ADD:
+			m_pListener->onCursorAdded(m_cursor);
+			break;
+
+		case CCursor::TS_UPDATE:
+			m_pListener->onCursorUpdated(m_cursor);
+			break;
+
+		case CCursor::TS_REMOVE:
+			m_pListener->onCursorRemoved(m_cursor);
+			break;
+
+		case CCursor::TS_UNDEFINE:
+			break;
+		}
+	}
+}
