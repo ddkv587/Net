1de26b38cf753348f0ee3adf794afe03b0963d61
ddkV587 <ddkV587@gmail.com>
2019-09-06 18:33:11 +0800

    update

--- include/Core/socket/IClientListener.hpp
+++ include/Core/socket/IClientListener.hpp
@@ -1,19 +1,19 @@
-#ifndef __IFILELISTENERHPP__
-#define __IFILELISTENERHPP__
+#ifndef __NET_ICLIENTLISTENER_HPP__
+#define __NET_ICLIENTLISTENER_HPP__
 
 //interface for the body which loop the client after accept form listener
 namespace NET
 {
-	class IClientListener
-	{
-		public:
-			virtual INT         addClient(INT) = 0;
-			virtual void        delClient(INT) = 0;
+    class IClientListener
+    {
+    public:// TODO: define your public method here
+        virtual INT         		addClient( INT ) = 0;
+        virtual void        		delClient( INT ) = 0;
 
-			virtual BOOLEAN     isEnable() = 0;
-			virtual BOOLEAN     isFull() = 0;
-		
-			virtual UINT        size() const = 0;
-	};
+        virtual BOOLEAN     		isEnable() = 0;
+        virtual BOOLEAN     		isFull() = 0;
+    
+        virtual UINT        		size() const = 0;
+    };
 }
-#endif
+#endif  // __NET_ICLIENTLISTENER_HPP__
